4c02bf5c84d958aa6bff372dc6aa0653efdf2ec3
Ray Tran <tranb2@spu.edu>
2019-06-11 18:23:50 -0700

    Added code to handle show film by id

--- public/utilities.js
+++ public/utilities.js
@@ -35,7 +35,45 @@ function PopulateFilmsTable(){
   xhttp.send();
 }
 
+function FindFilmByID() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function ReceivedCallback() {
+    if (this.readyState == 4 && this.status == 200) {
+      var responseJson = JSON.parse(this.responseText);
+      var tableBodyHTML = "";
+      //If a non-empty response comes back
+      if (responseJson.length > 0) {
+          tableBodyHTML += "<thead><tr><th scope=\"col\">Film ID</th><th scope=\"col\">Title</th><th scope=\"col\">Description</th>" +
+                           "<th scope=\"col\">Release Year</th><th scope=\"col\">Length</th><th scope=\"col\">Rating</th>" +
+                           "<th scope=\"col\">Category ID</th><th scope=\"col\">Last Updated</th></tr></thead><tbody>";
 
+           //Iterate through responseJson to format each record data into HTML string
+           for (record of responseJson) {
+               var formattedDate = new Date(record["last_update"]).toISOString().slice(0, 19).replace('T', ' ');
+               tableBodyHTML += "<tr>" +
+                                  "	<td>" + record["film_id"] + "</td>" +
+                                  "	<td>" + record["title"] + "</td>" +
+                                  "	<td>" + record["description"] + "</td>" +
+                                  "	<td>" + record["release_year"] + "</td>" +
+                                  "	<td>" + record["length"] + "</td>" +
+                                  "	<td>" + record["rating"] + "</td>" +
+                                  "	<td>" + record["category_id"] + "</td>" +
+                                  "	<td>" + formattedDate + "</td>" +
+                                "</tr>";
+           }
+           tableBodyHTML += "</tbody>";
+      }
+      // Else if nothing is retrieved
+      else {
+        // Display error message
+        document.getElementById("errorMessage").innerHTML = "No film with such ID";
+      }
+      document.getElementById("table").innerHTML = tableBodyHTML;
+    }
+  };
+  xhttp.open("GET", "api/films/" + document.getElementById("filmIDInput").value, true);
+  xhttp.send();
+}
 
 // Called by categories.html to populate table with all categories
 function PopulateCatsTable(){
@@ -68,8 +106,6 @@ function PopulateCatsTable(){
   xhttp.open("GET", "api/categories", true);
   xhttp.send();
 }
-
-
 
 // function CreateSelect(departments){
 // 	// <select id="department" name="department">
